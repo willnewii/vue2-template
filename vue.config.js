b2be39a72ab5407366adf1d59139344770fb6181
诗人的咸鱼 <willnewii@163.com>
2020-02-01 11:55:12 +0800

    change：vue.config.js->  transpileDependencies

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     publicPath: './',
     outputDir: 'dist',
+    transpileDependencies: ['vue-lib'],
     productionSourceMap: false,
     pluginOptions: {
         dll: {
@@ -45,17 +46,17 @@ module.exports = {
     chainWebpack: config => {
         //https://github.com/vuejs/vue-cli/issues/2843#issuecomment-458802639
         // 这里的 appStyles 中的 app 是入口文件的配置名称(从 vue inspect 中可以得到)
-        const splitOptions = config.optimization.get('splitChunks')
+        const splitOptions = config.optimization.get('splitChunks');
         splitOptions.cacheGroups.appStyles = {
             name: 'styles',
             test: (m, c, entry = 'app') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
             chunks: 'all',
             minChunks: 1,
             enforce: true
-        }
-        config.optimization.splitChunks(splitOptions)
+        };
+        config.optimization.splitChunks(splitOptions);
     }
-}
+};
 
 function recursiveIssuer(m) {
     if (m.issuer) {
