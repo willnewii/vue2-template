9351cdb543c31e13d0ed239ec36a88a88a973c72
诗人的咸鱼 <willnewii@163.com>
2020-02-02 13:24:13 +0800

    change：更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -44,8 +44,12 @@ module.exports = {
 
     },
     chainWebpack: config => {
-        //https://github.com/vuejs/vue-cli/issues/2843#issuecomment-458802639
-        // 这里的 appStyles 中的 app 是入口文件的配置名称(从 vue inspect 中可以得到)
+        // https://github.com/Yatoo2018/webpack-chain/tree/zh-cmn-Hans
+        /**
+         * 页面异步加载打包后，样式能否打包到一个css文件中
+         * https://github.com/vuejs/vue-cli/issues/2843#issuecomment-458802639
+         * 这里的 appStyles 中的 app 是入口文件的配置名称(从 vue inspect 中可以得到)
+         */
         const splitOptions = config.optimization.get('splitChunks');
         splitOptions.cacheGroups.appStyles = {
             name: 'styles',
@@ -55,6 +59,16 @@ module.exports = {
             enforce: true
         };
         config.optimization.splitChunks(splitOptions);
+
+        /**
+         * 移除console
+         * https://github.com/vuejs/vue-cli/issues/3088#issuecomment-572899228
+         * https://github.com/terser/terser#compress-options
+         */
+        config.optimization.minimizer('terser').tap((args) => {
+            args[0].terserOptions.compress.drop_console = true;
+            return args;
+        });
     }
 };
 
